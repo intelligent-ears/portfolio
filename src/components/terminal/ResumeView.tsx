b838147e92327e689f472c89334516b174ce1b70
K ARYA SEKHAR DAS <karyasdas2005@gmail.com>
2025-05-23 15:10:27 +0530

    Update ResumeView.tsx

--- src/components/terminal/ResumeView.tsx
+++ src/components/terminal/ResumeView.tsx
@@ -18,7 +18,7 @@ const ResumeView: React.FC = () => {
         </div>
         
         <a 
-          href="https://drive.google.com/file/d/1r2TyNtbMD7H7EsbEuXGAqeHuNt9XaiSz/view?usp=drive_link"
+          href="https://drive.google.com/file/d/15cUYwkyLFWl_VM84AULVGh5dMMJa-DZ5/view?usp=sharing"
           target="_blank"
           rel="noopener noreferrer"
           className="flex items-center bg-green-800 hover:bg-green-700 text-green-100 py-1 px-3 rounded text-sm focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50 transition duration-200"
